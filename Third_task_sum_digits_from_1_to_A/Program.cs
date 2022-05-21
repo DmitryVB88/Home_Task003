585196b79e620ed04dd2b6bac4a4fa328db94ae7
Dima <dima.brashovyan@mail.ru>
2022-05-21 22:10:57 +0300

    Сумма чисел от 1 до А

---
+++ Third_task_sum_digits_from_1_to_A/Program.cs
@@ -0,0 +1,10 @@
+﻿// Показать сумму чисел от 1 до А.
+Console.Clear();
+Console.Write("Введите число: ");
+int A = int.Parse(Console.ReadLine()?? "");
+int sum = 0;
+for(int i = 1; i <= A; i++)
+{
+    sum = sum + i;
+}
+Console.WriteLine(sum);
