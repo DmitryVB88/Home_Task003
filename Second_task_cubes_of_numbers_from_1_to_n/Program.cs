5664ec16f518c26d26bc873f7df9a0b9f29769de
Dima <dima.brashovyan@mail.ru>
2022-05-21 22:10:01 +0300

    таблица кубов чисел

---
+++ Second_task_cubes_of_numbers_from_1_to_n/Program.cs
@@ -0,0 +1,9 @@
+﻿// Найти кубы чисел от 1 до N
+Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine() ?? "");
+for(int i = 1; i <= n; i++)
+{
+    int cube = i * i * i;
+    Console.WriteLine($"Число {i} в третьей степени = {cube}");
+}
