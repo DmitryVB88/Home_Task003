dc404d7fd45ee5630696abd2d4d32584d82aebdf
Dima <dima.brashovyan@mail.ru>
2022-05-21 22:09:09 +0300

    Квадраты чисел

---
+++ First_task_square_numbers_from_1_to_n/Program.cs
@@ -0,0 +1,8 @@
+﻿// Показать таблицу квадратов чисел от 1 до N
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine() ?? "");
+for(int i = 1; i <= n; i++)
+{
+    int mult = i * i;
+    Console.Write($"{mult + " "}");
+}
