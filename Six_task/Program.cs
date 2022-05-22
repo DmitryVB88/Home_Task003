435b9b61553788e4a5673db992f5683aafe5beea
Dima <dima.brashovyan@mail.ru>
2022-05-22 21:28:00 +0300

    Таблица кубов - четные числа

---
+++ Six_task/Program.cs
@@ -0,0 +1,13 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()??"");
+int cube = 1;
+for(int i = 0; i <= n; i++)
+{
+    cube = i * i * i;
+    if (cube%2 == 0)
+    {
+        Console.Write($"{cube + " "}");
+    }
+}
