d4cf12957ef63d9fdfcd0b4b360b4f3388774c65
Dima <dima.brashovyan@mail.ru>
2022-05-22 22:16:43 +0300

    Возведение числа А в степень В

---
+++ Five_task/Program.cs
@@ -0,0 +1,23 @@
+﻿// Возведите число А в натуральную степень B используя цикл
+Console.Clear();
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine()?? "");
+Console.Write("Введите степень, в которую нужно возвести число: ");
+int b = int.Parse(Console.ReadLine() ?? "");
+int count = 1;
+int power = 1;
+while (count <= b)
+{
+    power = power * a;
+    count++;
+    
+}
+Console.WriteLine(power);
+
+// Второй вариант, через цикл for:
+
+for(int i = 1; i <= b; i++ )
+{
+    power = power * a; 
+    Console.WriteLine($"{power}");
+}
