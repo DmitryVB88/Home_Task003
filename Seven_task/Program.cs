01ee1fdafc845b6f517eb40df67b254bfa062267
Dima <dima.brashovyan@mail.ru>
2022-05-23 09:06:17 +0300

    Массив из 8-ми элементов, подсказака Стоуна

---
+++ Seven_task/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+Console.Clear();
+
+int[] array = new int[8];
+
+for(int i = 0; i < 8; i++)
+{
+    array[i] = new Random().Next(0, 2);
+}
+
+int count = 1;
+
+foreach(int n in array)
+{
+     Console.WriteLine($"{count} элемент массива равен {n}");
+    count++;
+}
+
